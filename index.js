9c136f7c2966b7eabe4b0ee1b0b52b2069465606
mdmorshedsagar <mdmosahidsagar5304257@gmail.com>
2023-05-19 15:00:17 -1200

    myToys-data-create

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const cors = require('cors');
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT ||5000;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
 //midlware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.3uy5gri.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,7 +21,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 // toyDogs
 //hFgT6SPhW19AmIip
@@ -32,43 +32,48 @@ async function run() {
     // Send a ping to confirm a successful connection
     const dbCollection = client.db("toyDogs").collection("subToyesData");
     const toyCollection = client.db("toyDogs").collection("ToyesData");
-    app.get('/allToys', async (req, res) => {
+    app.get("/allToys", async (req, res) => {
       const cursor = toyCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-  });
-  app.get('/singleToy/:id', async(req, res) => {
-    const id = req.params.id;
-    const result = await toyCollection.findOne({_id: new ObjectId(id)});
-    res.send(result);
-});
-    
-        app.post('/addToy', async (req, res) => {
-            const newToy = req.body;
-            console.log(newToy);
-            const data = await toyCollection.insertOne(newToy);
-            res.send(data);
+    });
+    app.get("/singleToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await toyCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+    app.get("/myToys/:email", async (req, res) => {
+      const result = await toyCollection
+        .find({
+          email: req.params.email,
         })
-   
+        .toArray();
+      res.send(result);
+    });
+
+    app.post("/addToy", async (req, res) => {
+      const newToy = req.body;
+      console.log(newToy);
+      const data = await toyCollection.insertOne(newToy);
+      res.send(data);
+    });
+
     app.get("/:category", async (req, res) => {
-        
-        const data = await dbCollection.find({
-            category: req.params.category,
-          })
-          .toArray();
-        res.send(data);
+      const data = await dbCollection
+        .find({
+          category: req.params.category,
+        })
+        .toArray();
+      res.send(data);
+    });
+
+    app.get("/singleSubToy/:id", async (req, res) => {
+      const jobs = await dbCollection.findOne({
+        _id: new ObjectId(req.params.id),
       });
-    
-   
-      app.get("/singleSubToy/:id", async (req, res) => {
-       
-        const jobs = await dbCollection.findOne({
-          _id: new ObjectId(req.params.id),
-        });
-        res.send(jobs);
-      });
+      res.send(jobs);
+    });
     await client.db("admin").command({ ping: 1 });
-  
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -76,7 +81,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
